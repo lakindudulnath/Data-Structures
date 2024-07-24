aaa721e9e1294ca934d8c0bebae04ac3d9fb602a
lakindudulnath <lakindu.dulnath.123@gmail.com>
2024-07-24 06:05:31 +0530

    Add files via upload

---
+++ Steque_Implementation/src/Steque/StequeUsingTwoStacks.java
@@ -0,0 +1,54 @@
+package Steque;
+
+import java.util.Stack;
+
+public class StequeUsingTwoStacks<T> {
+    private Stack<T> stack1;
+    private Stack<T> stack2;
+
+    public StequeUsingTwoStacks() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(T element) {
+        stack1.push(element);
+    }
+
+    public void enqueue(T element) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack1.push(element);
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Steque is empty. Cannot pop element.");
+        }
+        return stack1.pop();
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty();
+    }
+
+    public int size() {
+        return stack1.size();
+    }
+
+    public static void main(String[] args) {
+        StequeUsingTwoStacks<Integer> steque = new StequeUsingTwoStacks<>();
+        steque.push(10);
+        steque.push(20);
+        steque.enqueue(30);
+        
+        System.out.println("Popped element: " + steque.pop());
+        System.out.println("Is the steque empty? " + steque.isEmpty());
+        System.out.println("Size of the steque: " + steque.size());
+    }
+}
+
---
+++ Steque_Implementation/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Steque_Implementation {
+}
